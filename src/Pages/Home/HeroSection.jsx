2ab38cbb90f32a0a1e2e1b8546abcf22f558419d
EstebanGC <johan.gal.car@gmail.com>
2023-10-11 21:08:21 -0500

    Initial commit

---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,24 @@
+export default function HeroSection() {
+    return (
+        <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Esteban</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Software Developer</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+            Desarrollador enfocado al backend con manejo de Java, Node, Python.
+            <br /> Gracias por visitar, te mostraré un poco de mi trabajo.
+          </p>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img.png" alt="Hero Section" />
+      </div>
+    </section>
+    )
+}
